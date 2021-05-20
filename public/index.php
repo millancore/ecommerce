bdb3bd4c59c94e444b93fc9271256163adf53ac8
Juan Millan <juanes14569@gmail.com>
2021-05-20 19:39:30 -0300

    Test with some core libraries

--- public/index.php
+++ public/index.php
@@ -2,4 +2,62 @@
 
 include_once __DIR__. '/../bootstrap.php';
 
-echo "Vamos de compras";
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+# Services Container
+
+$request = Request::createFromGlobals();
+
+// Router
+  # -> Router Collection
+  # -> Matcher
+  # -> Controller Dispatcher
+
+// map a route
+
+class HomeController {
+    public function index() {
+        echo 'Hello!';
+    }
+}
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', [HomeController::class, 'index']);
+});
+
+$httpMethod = $request->getMethod();
+$uri = $request->getRequestUri();
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        break;
+}
+
+$controller = new $handler[0];
+$controller->{$handler[1]}();
+
+# Controllers
+
+  # Model - ORM
+  # Views
+
+# Response
